4e9a9f02de612f9a6b3b2a4e9e90a7f38bcf7195
Валерия Никитина <valliejanen@gmail.com>
2019-12-16 20:44:56 +0300

    сcommit

--- MoodTracker2.0/MoodTracker2.0/MainWindow.xaml.cs
+++ MoodTracker2.0/MoodTracker2.0/MainWindow.xaml.cs
@@ -20,9 +20,14 @@ namespace MoodTracker2._0
     /// </summary>
     public partial class MainWindow : Window
     {
+ 
         public MainWindow()
         {
             InitializeComponent();
+
+
         }
+
+
     }
 }
